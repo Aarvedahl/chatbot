2cf5c499f62a22fc02064ab341aa5a5a49327325
Alex <alex.arvedahl@gmail.com>
2018-01-01 15:16:52 +0100

    Added /caps function

--- bot.py
+++ bot.py
@@ -1,25 +1,25 @@
 from telegram.ext import Updater, CommandHandler
 from telegram.ext import MessageHandler, Filters
-'''
-def hello(bot, update):
-    update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
-'''
+
 def start(bot, update):
        bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
 updater = Updater('462046710:AAFS-Xk2jtsGuyyCZa5jqNN5KRxwntSi5ro')
 
-
 def echo(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+def caps(bot, update, args):
+    text_caps=' '.join(args).upper()
+    bot.send_message(chat_id=update.message.chat_id, text=text_caps)
 
 start_handler = CommandHandler('start', start)
 dispatcher = updater.dispatcher
 dispatcher.add_handler(start_handler)
 echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(echo_handler)
-#updater.dispatcher.add_handler(CommandHandler('hello', hello))
+caps_handler = CommandHandler('caps', caps, pass_args=True)
+dispatcher.add_handler(caps_handler)
 
 updater.start_polling()
 updater.idle()
