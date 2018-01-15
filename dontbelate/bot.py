8abca206302ef6d68e68da7fa038e28dfea66d04
Alex <alex.arvedahl@gmail.com>
2018-01-15 08:29:39 +0100

    Draft for /start command

--- dontbelate/bot.py
+++ dontbelate/bot.py
@@ -2,3 +2,19 @@
 # /setdestination feature
 # /transportation feature
 # Also it will ask what time the bus or train leaves to know which exactly you want to catch
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Welcome! I am DoNotBeLate bot. /n What is your home address? /n What is your preferred way of transportation? Walk, Cycle or Car /n What is usually your destination?")
+
+updater = Updater("token")
+
+start_handler = CommandHandler('start', start)
+dispatcher = updater.dispatcher
+dispatcher.add(start_handler)
+
+updater.start_polling()
+updater.idle()
+
