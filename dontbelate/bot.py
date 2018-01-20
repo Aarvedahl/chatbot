d23bb1139d7b03762df7e017cb9f6987d5f3c0f5
Alex <alex.arvedahl@gmail.com>
2018-01-20 16:25:27 +0100

    New better draft for dontbelate

--- dontbelate/bot.py
+++ dontbelate/bot.py
@@ -3,18 +3,17 @@
 # /transportation feature
 # Also it will ask what time the bus or train leaves to know which exactly you want to catch
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telebot
 
+bot = telebot.TeleBot("TOKEN")
 
-def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Welcome! I am DoNotBeLate bot. /n What is your home address? /n What is your preferred way of transportation? Walk, Cycle or Car /n What is usually your destination?")
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
 
-updater = Updater("token")
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+    # Bygg en egen phrase sorter here and if phrase not recognized show examples
 
-start_handler = CommandHandler('start', start)
-dispatcher = updater.dispatcher
-dispatcher.add(start_handler)
-
-updater.start_polling()
-updater.idle()
-
+bot.polling()
